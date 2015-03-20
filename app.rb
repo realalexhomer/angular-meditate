963700235a910f70f7cda6d2c8812af12586cfcc
realalexhomer <realalexhomer@gmail.com>
2015-03-19 22:35:44 -0400

    initial commit

---
+++ app.rb
@@ -0,0 +1,29 @@
+# encoding: utf-8
+require 'multi_json'
+require 'sinatra'
+require 'sinatra/activerecord'
+
+class Myapp < Sinatra::Application
+  enable :sessions
+  set :session_secret, 'alexlovestomeditatealldayandallnight'
+
+  configure :development do
+    set :database, "sqlite3:////tmp/my.db"
+    if (RUBY_PLATFORM == "java")
+      require 'jdbc/sqlite3'
+      Jdbc::SQLite3.load_driver
+    end
+  end
+
+  configure :production do
+    set :database, 'postgres://postgres:12345@localhost/sinatra_service'
+    if (RUBY_PLATFORM == "java")
+      require 'jdbc/postgres'
+      Jdbc::Postgres.load_driver
+    end
+  end
+end
+
+require_relative 'helpers/init'
+require_relative 'models/init'
+require_relative 'routes/init'
